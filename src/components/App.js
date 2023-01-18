1e44c4f94b73d06dbc5d5e708d93bbc17319987e
yurubahmed <yurubiftin2002@gmail.com>
2023-01-18 10:07:27 -0500

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+ 
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
@@ -7,7 +8,27 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      {/*{code here}*/}
+      <NavBar />
+      <Switch>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/directors">
+          <Directors />
+        </Route>
+        <Route path="/actors">
+          <Actors />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
+
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,26 @@
+
 import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((director) => {
+        return (
+          <div key={director.name}>
+            <h2>{director.name}</h2>
+            <ul>
+              {director.movies.map((movie) => {
+                return <li key={movie}>{movie}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Directors;
